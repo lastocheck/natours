d3e91ba0eba19f938bf7fab1b107e6c78f56f576
lastocheck <lastocheck@gmail.com>
2020-11-29 11:27:04 +0300

    Implement advanced filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -7,7 +7,13 @@ exports.getAllTours = async (req, res) => {
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         excludedFields.forEach((el) => delete queryObj[el]);
 
-        const query = Tour.find(queryObj);
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(
+            /\b(gte|gt|lte|lt)\b/g,
+            (match) => `$${match}`
+        );
+
+        const query = Tour.find(JSON.parse(queryStr));
 
         // Execute query
         const tours = await query;
