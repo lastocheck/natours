29f3c67ad4611a026a23fbd99c7b5cc8e616d7b4
lastocheck <lastocheck@gmail.com>
2020-11-29 17:08:47 +0300

    Add tour stats and monthly plan aggregators

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -101,3 +101,85 @@ exports.deleteTour = async (req, res) => {
         });
     }
 };
+
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte: 4.5 } },
+            },
+            {
+                $group: {
+                    _id: { $toUpper: '$difficulty' },
+                    numTours: { $sum: 1 },
+                    numRatings: { $sum: '$ratingsQuantity' },
+                    avgRating: { $avg: '$ratingsAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' },
+                },
+            },
+            {
+                $sort: { avgPrice: 1 },
+            },
+            // { $match: { _id: { $ne: 'EASY' } } },
+        ]);
+
+        res.status(200).json({
+            status: 'success',
+            data: stats,
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: error.message,
+        });
+    }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+    try {
+        const year = Number(req.params.year);
+        const plan = await Tour.aggregate([
+            {
+                $unwind: '$startDates',
+            },
+            {
+                $match: {
+                    startDates: {
+                        $gt: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`),
+                    },
+                },
+            },
+            {
+                $group: {
+                    _id: { $month: '$startDates' },
+                    numTourStarts: { $sum: 1 },
+                    tours: { $push: '$name' },
+                },
+            },
+            {
+                $sort: { numTourStarts: -1 },
+            },
+            {
+                $addFields: { month: '$_id' },
+            },
+            {
+                $project: { _id: 0 },
+            },
+            {
+                $limit: 6,
+            },
+        ]);
+        res.status(200).json({
+            status: 'success',
+            data: plan,
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            message: error.message,
+        });
+    }
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,11 +6,16 @@ const {
     updateTour,
     deleteTour,
     aliasTopTours,
+    getTourStats,
+    getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
